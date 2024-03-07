f6b4100dd49cad85de21dbd2d3800537d8ffe533
Jeiss Varghese <113582698+jeiss5349@users.noreply.github.com>
2024-03-06 19:18:14 -0500

    added the home view inside the views

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <NavBar />
+  </header>
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,16 +1,27 @@
-<template>
- <nav-bar></nav-bar>
-</template>
+<script setup lang="ts">
 
-<script>
-import NavBar from "../components/NavBar.vue"
-export default {
-components:{
-    NavBar
-}
-}
 </script>
 
-<style>
-
+<template>
+  <main>
+<div class="hero-body">
+  <div class ="container">
+    <div class = "columns is-cventered">
+      <div class = "column is-one-third">
+        <p class="title is-1">
+          Let's Get Started
+        </p>
+        <p class="subtitle">
+          Start being Healthy
+        </p>
+      </div>
+      <div class = "column is-one-half">
+        <img src = "../assets/Breakfast.jpg" width = "2000em" height = "200em">
+      </div>
+    </div>
+  </div>
+</div>
+  </main>
+</template>
+<style scoped>
 </style>
