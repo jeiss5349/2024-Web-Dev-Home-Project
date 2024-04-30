1c569cf5220c750fd90df3b67e0a09717ce71910
jeiss5349 <varghesj4@newpaltz.edu>
2024-04-30 15:28:35 +0500

    added error message view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
+import MessageListView from "./views/MessageListView.vue";
+
 </script>
 
 <template>
   <header>
     <NavBar />
+    <MessageListView />
   </header>
   <div class="container">
     <RouterView />
